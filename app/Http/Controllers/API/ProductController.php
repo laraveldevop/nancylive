49c8d5ab238681c75e8013c8b41e92df4e9448fa
Krishana <kisupatel1304@gmail.com>
2020-09-02 19:44:11 +0530

    change in api

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Product;
 use App\Video;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
@@ -20,6 +21,13 @@ class ProductController extends Controller
     {
         if ($request['id'] != null) {
             $products = Product::where('id', $request['id'])->get();
+            foreach ($products as $item) {
+                $qu= DB::table('product_image')
+                    ->select(array('id','image'))
+                    ->where('product_id',$item->id)
+                    ->get();
+                $item['images']= $qu;
+            }
             return response()->json(['status' => true, 'message' => 'Products retrieved successfully.', 'data' => $products,], 200);
         } else {
             $products = Product::all();
