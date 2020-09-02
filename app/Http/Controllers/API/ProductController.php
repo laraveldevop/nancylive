7a996fb815a3f929fc81bc4d297f693b9fdcf25c
Krishana <kisupatel1304@gmail.com>
2020-09-02 11:51:34 +0530

    change in api

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -18,14 +18,6 @@ class ProductController extends Controller
      */
     public function index(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'id' => 'required|numeric',
-        ]);
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => false,
-                'message' => $validator->errors()], 401);
-        }
         if ($request['id'] != null) {
             $products = Product::where('id', $request['id'])->get();
             return response()->json(['status' => true, 'message' => 'Products retrieved successfully.', 'data' => $products,], 200);
--- app/Http/Controllers/API/VideoController.php
+++ app/Http/Controllers/API/VideoController.php
@@ -11,14 +11,7 @@ class VideoController extends Controller
 {
     public function index(Request  $request)
     {
-        $validator = Validator::make($request->all(), [
-            'id' => 'required|numeric',
-        ]);
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => false,
-                'message' => $validator->errors()], 401);
-        }
+
         if ($request['id'] != null) {
             $video = Video::where('id', $request['id'])->get();
             return response()->json(['status' => true, 'message' => 'Video retrieved successfully.', 'data' => $video,], 200);
