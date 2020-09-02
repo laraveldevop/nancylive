26b3c3c7b97d4dd64b18d949174e6e926050b8b4
Krishana <kisupatel1304@gmail.com>
2020-09-02 11:46:25 +0530

    change in api

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -4,21 +4,37 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Product;
+use App\Video;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
+     * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::all();
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|numeric',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => $validator->errors()], 401);
+        }
+        if ($request['id'] != null) {
+            $products = Product::where('id', $request['id'])->get();
+            return response()->json(['status' => true, 'message' => 'Products retrieved successfully.', 'data' => $products,], 200);
+        } else {
+            $products = Product::all();
 
 
-        return response()->json(['status'=>true,'message'=>'Products retrieved successfully.' ,'data'=>$products->toArray(), ],200);
+            return response()->json(['status' => true, 'message' => 'Products retrieved successfully.', 'data' => $products->toArray(),], 200);
+        }
     }
 
     /**
--- app/Http/Controllers/API/VideoController.php
+++ app/Http/Controllers/API/VideoController.php
@@ -5,14 +5,31 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Video;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class VideoController extends Controller
 {
-    public function index()
+    public function index(Request  $request)
     {
-        $video = Video::all();
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|numeric',
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => $validator->errors()], 401);
+        }
+        if ($request['id'] != null) {
+            $video = Video::where('id', $request['id'])->get();
+            return response()->json(['status' => true, 'message' => 'Video retrieved successfully.', 'data' => $video,], 200);
+        }
+        else {
+            $video = Video::all();
 
 
-        return response()->json(['status'=>true,'message'=>'Video retrieved successfully.' ,'data'=>$video->toArray(), ],200);
+            return response()->json(['status' => true, 'message' => 'Video retrieved successfully.', 'data' => $video->toArray(),], 200);
+        }
     }
+
+
 }
