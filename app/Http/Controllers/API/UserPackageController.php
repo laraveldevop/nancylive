1214e64e57c27dd2a350df033dd4d862b5d5618a
Krishana <kisupatel1304@gmail.com>
2020-09-24 11:49:57 +0530

    change in user package

--- app/Http/Controllers/API/UserPackageController.php
+++ app/Http/Controllers/API/UserPackageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Order;
+use App\Package;
 use App\UserPackage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -23,12 +24,51 @@ class UserPackageController extends Controller
                 'status'=> false,
                 'message'=>$validator->errors()->all()], 422);
         }
+        $package= Package::where('id',$request['package_id'])->first();
 
         $userPackage =new UserPackage();
         $userPackage->user_id = $request['user_id'];
         $userPackage->package_id = $request['package_id'];
+        $userPackage->video_count = $package['content_count'];
+        if(!empty($package['year'])) {
+            $userPackage->expire_date = date('Y-m-d', strtotime($package['created_at'] . '+ ' . $package['year'] . ' year'));
+        }elseif (!empty($package['month'])) {
+            $userPackage->expire_date = date('Y-m-d', strtotime($package['created_at'] . '+ ' . $package['month'] . ' month'));
+        }else {
+            $userPackage->expire_date = date('Y-m-d', strtotime($package['created_at'] . '+ ' . $package['day'] . ' day'));
+        }
+        if (!empty($package['category_id'])) {
+            $userPackage->stat = 2;
+            $userPackage->category_id = $package['category_id'];
+        }elseif (empty($package['category_id'] && $package['content_count'])) {
+            $userPackage->stat = 1;
+        }else {
+            $userPackage->stat = 3;
+            $userPackage->video_count = $package['content_count'];
+
+        }
         $userPackage->save();
         return response()->json(['status'=>true,'message'=>'User Package Create successfully.' ,'data'=>$userPackage ],200);
+
+    }
+
+    public function userPackageUpdate(Request $request){
+        $validator = Validator::make($request->all(),[
+            'user_id' => ['required', 'numeric'],
+            'video_id' => ['required', 'numeric'],
+            'package_id' => ['required', 'numeric'],
+        ]);
+
+        if ($validator->fails())
+        {
+            return response()->json([
+                'status'=> false,
+                'message'=>$validator->errors()->all()], 422);
+        }
+        $user = UserPackage::where('user_id',$request['user_id'])->first();
+        $user->video_id = $request['video_id'];
+        $user->save();
+        return response()->json(['status'=>true,'message'=>'User Package Create successfully.' ,'data'=>$user ],200);
 
     }
 }
