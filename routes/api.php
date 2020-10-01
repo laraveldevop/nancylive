404d533a19f65edff93ec28ac57ae3035a8fad51
Krishana <kisupatel1304@gmail.com>
2020-10-01 18:52:43 +0530

    change in api route

--- routes/api.php
+++ routes/api.php
@@ -37,16 +37,11 @@ Route::group(['middleware' => ['json.response']], function () {
     Route::post('/password/reset', 'Api\ResetPasswordController@reset');
 
 
-    Route::get('home', 'API\HomeController@advertise')->middleware('checkUser');
     Route::post('category-detail', 'API\CategoryViewController@categoryDetail')->middleware('checkUser');
     Route::post('brand-detail', 'API\BrandViewController@brandDetail')->middleware('checkUser');
     Route::post('sponsor-detail', 'API\SponsorViewController@sponsorDetail')->middleware('checkUser');
     Route::post('artist-detail', 'API\HomeController@artist')->middleware('checkUser');
-    Route::get('order-detail', 'API\HomeController@order')->middleware('checkUser');
-    Route::get('artist', 'API\HomeController@index')->middleware('checkUser');
     Route::post('product', 'API\ProductController@index')->middleware('checkUser');
-    Route::get('document', 'API\PdfController@index')->middleware('checkUser');
-    Route::get('history', 'API\HistoryController@index')->middleware('checkUser');
     Route::post('user-package-history', 'API\HistoryController@userPackageHistory')->middleware('checkUser');
     Route::post('video', 'API\VideoController@index')->middleware('checkUser');
     Route::post('user-update', 'API\HomeController@userUpdate')->middleware('checkUser');
@@ -54,8 +49,14 @@ Route::group(['middleware' => ['json.response']], function () {
     Route::post('user-package', 'API\UserPackageController@userPackage')->middleware('checkUser');
     Route::post('package-update', 'API\UserPackageController@userPackageUpdate')->middleware('checkUser');
     Route::post('change-password', 'API\HomeController@changePassword')->middleware('checkUser');
-    Route::get('package', 'API\PackageController@index')->middleware('checkUser');
     Route::post('check_user', 'API\AuthController@check_user');
+
+    Route::get('home', 'API\HomeController@advertise')->middleware('checkUser');
+    Route::get('order-detail', 'API\HomeController@order')->middleware('checkUser');
+    Route::get('artist', 'API\HomeController@index')->middleware('checkUser');
+    Route::get('document', 'API\PdfController@index')->middleware('checkUser');
+    Route::get('history', 'API\HistoryController@index')->middleware('checkUser');
+    Route::get('package', 'API\PackageController@index')->middleware('checkUser');
 
     // private routes
     Route::middleware('auth:api')->group(function () {
