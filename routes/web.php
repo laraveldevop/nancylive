ff1eec6495a734babfda845badd59344a45777bd
Krishana <kisupatel1304@gmail.com>
2020-11-05 09:45:30 +0530

    change in route

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes(['register' => false]);
 
-Auth::routes();
+//Auth::routes();
 
+Auth::routes([ 'register' => false ]);
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 
