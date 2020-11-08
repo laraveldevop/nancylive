ec7ab3b7452110f205ecb83d59da5c90edf11828
Krishana <kisupatel1304@gmail.com>
2020-11-08 22:19:56 +0530

    fix bug

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -239,6 +239,7 @@ class ProductController extends Controller
         }
         $images = $request->file('files');
         if ($request->hasFile('files')) :
+            $request->validate(['files'=>'mimes:jpg,jpeg,png']);
             foreach ($images as $item):
 
                 $path = Storage::disk('public')->put('product_images', $item);
