e738c3d84d2cefcf67f0a702e6f8c7f9f6db68ca
Krishana <kisupatel1304@gmail.com>
2021-01-12 22:49:22 +0530

    fix bug

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -8,10 +8,11 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $v=[];
-        $user=  User::all();
+        $id= $request->header('USER_ID');
+        $user=  User::where('id',$id)->get();
         foreach($user as $item){
             $value =$item->roles->first();
             $item['role'] = isset($value['name'])?$value['name']: null;
