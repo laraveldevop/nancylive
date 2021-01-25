80eaf859799cfd5bebfda22a88d5a934c8e78586
Krishana <kisupatel1304@gmail.com>
2021-01-25 13:05:58 +0530

    add sub api and fix bug

--- app/Http/Controllers/API/ReferralController.php
+++ app/Http/Controllers/API/ReferralController.php
@@ -12,12 +12,17 @@ class ReferralController extends Controller
 {
     public function referral()
     {
+        $v = [];
         $referral = Referral::select(DB::raw('users.*,referral.status,referral.user_id'))->leftjoin('users','referral.referral_code','=','users.referral_code')->get();
+        $user = $referral->unique('referral_code');
+        $user->all();
         foreach ($referral as $item){
-          $user =   User::where('id', $item->user_id)->get();
-            $item['users'] = $user;
+          $users =   User::select('id')->where('id', $item->user_id)->get();
+            array_push($v,$users);
+            $user['users'] = $v;
         }
-        return response()->json(['status' => true, 'message' => 'Data Retrieve Successfully', 'data' => $referral],200);
+
+        return response()->json(['status' => true, 'message' => 'Data Retrieve Successfully', 'data' => $user],200);
 
     }
 }
---
+++ app/Http/Controllers/API/SubRoleController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\SubRole;
+use App\User;
+use Illuminate\Http\Request;
+
+class SubRoleController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'role' => ['required'],
+        ]);
+        $id = $request->input('id');
+        if ($id == null) {
+            $news = $request->input('role');
+            $subRole = new SubRole();
+            $subRole->role = $news;
+            $subRole->save();
+            return response()->json(['status' => true, 'message' => 'Add Successfully', 'data' => $subRole]);
+        }
+        else{
+            $subRole = SubRole::find($id);
+            $subRole->role = $request->input('role');
+            $subRole->save();
+            return response()->json(['status' => true, 'message' => 'Update Successfully', 'data' => $subRole]);
+        }
+    }
+
+    public function delete(Request $request)
+    {
+        $id = $request->input('id');
+        User::where('sub_role_id', $id)->update(['sub_role_id' => null]);
+        SubRole::destroy($id);
+        return response()->json(['status' => true, 'message' => 'Delete Successfully', 'data' =>[]]);
+
+    }
+}
