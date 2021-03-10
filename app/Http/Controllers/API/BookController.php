e76fe819e8c43268c5b2ab2d7a754e679964695c
Krishana <kisupatel1304@gmail.com>
2021-03-10 17:47:57 +0530

    fix bug

--- app/Http/Controllers/API/BookController.php
+++ app/Http/Controllers/API/BookController.php
@@ -33,9 +33,9 @@ class BookController extends Controller
             $book->status = $request->input('status');
             $book->transaction_id = $request->input('transaction_id');
             $book->save();
-            return response()->json(['status' => true, 'message' => 'Add Successfully', 'data' => $book],200);
+            return response()->json(['status' => true, 'message' => 'Add Successfully', 'data' => $book], 200);
 
-        }else{
+        } else {
             $book = Book::find($id);
             $book->ticket_id = $request->input('ticket_id');
             $book->user_id = $request->input('user_id');
@@ -43,11 +43,10 @@ class BookController extends Controller
             $book->status = $request->input('status');
             $book->transaction_id = $request->input('transaction_id');
             $book->save();
-            return response()->json(['status' => true, 'message' => 'Update Successfully', 'data' => $book],200);
+            return response()->json(['status' => true, 'message' => 'Update Successfully', 'data' => $book], 200);
 
         }
     }
-
 
 
     public function showList(Request $request)
@@ -62,32 +61,35 @@ class BookController extends Controller
         }
         $user_id = $request->input('user_id');
         $booked = $request->input('booked');
-       if ($user_id != null && $booked == null){
-//            $showlist = Book::select(DB::raw('ticket.*'))->leftjoin('ticket','book.ticket_id','ticket.id')->where('user_id', $user_id)->first();
-            $showlist = Ticket::select(DB::raw('ticket.*'))->leftjoin('book','book.ticket_id','ticket.id')->where('book.user_id', $user_id)->first();
-            if (!empty($showlist)){
-                $showlist['booked'] = 1;
-            }
-            else{
-                $showlist = Ticket::select(DB::raw('ticket.*'))->leftjoin('book','book.ticket_id','!=','ticket.id')->first();
-                $showlist['booked'] = 0;
+        if ($user_id != null && $booked == null) {
+            $showlist = Ticket::all();
+            $book = Book::all();
 
+
+            foreach ($showlist as $value) {
+                foreach ($book as $item) {
+                    if ($value->id == $item->ticket_id && $user_id == $item->user_id) {
+                        $value['booked'] = 1;
+                        break;
+                    } else {
+                        $value['booked'] = 0;
+                    }
+                }
             }
+
             return response()->json(['status' => true, 'message' => 'Available Data', 'data' => $showlist], 200);
-        }
-        elseif ($user_id != null && $booked != null){
+        } elseif ($user_id != null && $booked != null) {
 
             if ($booked == 1) {
-                $showlist = Book::select(DB::raw('ticket.*'))->leftjoin('ticket','book.ticket_id','ticket.id')->where('user_id', $user_id)->get();
+                $showlist = Book::select(DB::raw('ticket.*'))->leftjoin('ticket', 'book.ticket_id', 'ticket.id')->where('user_id', $user_id)->get();
                 return response()->json(['status' => true, 'message' => 'Available Booked Show', 'data' => $showlist], 200);
-            }else{
-                $showlist = Book::select(DB::raw('ticket.*'))->join('ticket','ticket.id','!=','book.ticket_id')->get();
+            } else {
+                $showlist = Book::select(DB::raw('ticket.*'))->join('ticket', 'ticket.id', '!=', 'book.ticket_id')->get();
                 return response()->json(['status' => true, 'message' => 'Not Booked Show', 'data' => $showlist], 200);
             }
-        }
-        else{
+        } else {
             $showlist = Ticket::all();
-            return response()->json(['status' => true, 'message' => 'Get All Show', 'data' => $showlist],200);
+            return response()->json(['status' => true, 'message' => 'Get All Show', 'data' => $showlist], 200);
 
         }
 
@@ -104,8 +106,8 @@ class BookController extends Controller
                 'message' => $validator->errors()], 422);
         }
         $ticket_id = $request->input('ticket_id');
-        $bookNow = Book::where('ticket_id',$ticket_id)->get();
-        return response()->json(['status' => true, 'message' => 'Data', 'data' => $bookNow],200);
+        $bookNow = Book::where('ticket_id', $ticket_id)->get();
+        return response()->json(['status' => true, 'message' => 'Data', 'data' => $bookNow], 200);
 
     }
 
